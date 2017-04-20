00b5151bd3b9ba1536211beede3c0ad7d2bb70fa
Eric Takahashi <etakahashi@finchcomputing.com>
2017-04-20 10:48:19 -0700

    Documentation updates.

--- mathext.go
+++ mathext.go
@@ -1,10 +1,8 @@
 package mathext
 
-// Hashes a string into an int.  This operation is useful if you want to convert
-// a weighted string vector into a SparseVector. I.e. each string s[i] in a
-// weighted vector V=[s0w0, s1w1, ...] can be converted into an int.
+// Hashes a string into an int.
 func Hash(s string) int {
-	// NOTE: this implementation based on Java's String.hashCode()
+	// NOTE: This implementation based on Java's String.hashCode()
 
 	h := 1125899906842597 // prime
 	len := len(s)
